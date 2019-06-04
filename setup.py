2e50f5f9824b96ec82d84fbf72ae62d30edde868
zwb <zhuangweibin@stbl.cc>
2019-06-04 11:36:56 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zwb_image",
+    version="1.0.0",
+    keywords=("util"),
+    description="zwb_image",
+    long_description="zwb_image",
+    url="https://github.com/weibin268/zwb_image",
+    author="zhuang weibin",
+    author_email="448075543@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
